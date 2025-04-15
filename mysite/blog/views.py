d24c8eea6d05477e3e4623e206a69c0b7cd77108
stefandobrovolskiy <stefan.o.dobrovolskyi@ukd.edu.ua>
2025-04-15 09:26:46 +0300

    PR_APLIKACIA

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,7 @@
+# blog/views.py
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class HomePageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'blog/index.html')
